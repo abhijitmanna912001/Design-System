9c92636018bfec717ebcd76e4b2344334fd61ff1
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2025-05-21 16:11:42 +0530

    Building Foundation Package

---
+++ packages/foundation/lib/index.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.tokens = void 0;
+var tokens_1 = __importDefault(require("tokens/js/tokens"));
+exports.tokens = tokens_1.default;
+//# sourceMappingURL=index.js.map
